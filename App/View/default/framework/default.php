37b2ec54df42d5cfdbcef18e77f14a10539aabb9
Paul Dragoonis <dragoonis@gmail.com>
2011-10-30 17:22:13 +0000

    Updating styles for the skeleton app's default loading page

--- App/View/default/framework/default.php
+++ App/View/default/framework/default.php
@@ -1,9 +1,21 @@
-<p style="font-size: 15px;">Welcome to the PPI Skeleton Appication</p>
-<p style="font-size: 15px;">You are here because this is the default <b>route</b> for the application</p>
-<p style="font-size: 15px;">Your current configured baseUrl is: <?= $baseUrl; ?></p>
-<p style="font-size: 15px;">You must remove your default route from your routes file which is here:<br>
-	<b><?= APPFOLDER . 'Config/routes.php'; ?></b>
+<h1>Welcome to the PPI Skeleton Appication</h1>
+<p>You are here because this is the default <b>route</b> for the application</p>
+<p>Your current configured baseUrl is: <?= $baseUrl; ?></p>
+<br>
+<p>You must remove your default route from your routes file which is here:<br>
+<b><?= APPFOLDER . 'Config/routes.php'; ?></b>
 </p>
-<p style="font-size: 15px;">Your must update your baseUrl in your application's config file which is here:<br>
+<br>
+<p>Your must update your baseUrl in your application's config file which is here:<br>
 	<b><?= APPFOLDER . 'Config/general.ini'; ?></b>
 </p>
+
+<style type="text/css">
+h1 {
+    font-size: 14px;
+    margin-bottom: 12px;
+}
+p {
+    color: black;
+}
+</style>
