12445ab8b7030e932fa104a23e27ee22f31bbdc9
Paul Dragoonis <dragoonis@gmail.com>
2011-10-30 17:22:59 +0000

    Adding the default index and config files

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 date_default_timezone_set('Europe/London');
-require_once '../ppi-framework/PPI/init.php';
-$app = new \PPI\App();
+require_once 'PPI/PPI/init.php';
+$app = new PPI\App();
 $app->boot()->dispatch();
