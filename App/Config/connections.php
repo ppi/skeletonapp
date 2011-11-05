b8ea6c8f32111236b94f166f0e84e2f875c1b694
Paul Dragoonis <dragoonis@gmail.com>
2011-11-05 16:27:10 +0000

    Adding datasource references to the skeleton app

---
+++ App/Config/connections.php
@@ -0,0 +1,10 @@
+<?php
+$connections = array();
+
+$connections['main'] = array(
+	'type'   => 'pdo_mysql',
+	'host'   => 'localhost',
+	'dbname' => 'ppiapp',
+	'user'   => 'root',
+	'pass'   => ''
+);
--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 date_default_timezone_set('Europe/London');
 require_once 'PPI/PPI/init.php';
 $app = new PPI\App();
+
+// Uncomment to use the PPI\DataSource component
+//$app->ds = true;
+
 $app->boot()->dispatch();
