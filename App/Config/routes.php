9f289a955a10a82823fa47126a1dd0238f38ed3c
Paul Dragoonis <dragoonis@gmail.com>
2012-01-05 00:21:07 +0000

    Removing old routes from the routes.php file

--- App/Config/routes.php
+++ App/Config/routes.php
@@ -1,16 +1,4 @@
 <?php
 $routes = array();
-
-// --- DEFAULT SECTION - REMOVE THIS ---
-$routes['.*'] = 'general/showdefault';
-return;
-// --- DEFAULT SECTION - REMOVE THIS ---
-
 $routes['__default__'] = 'home/index';
 $routes['__404__'] = 'general/show404';
-
-// About Match
-$routes['/about\.html'] = 'general/about';
-$routes['/advertising\.html'] = 'general/advertising';
-// Page Match
-$routes['/:any/:any/(:any)\.html\?id=(:num)'] = 'page/view/$2/$1';
