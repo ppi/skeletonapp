bbec5cec40385518979525867abcdb8b812566a0
Paul Dragoonis <dragoonis@gmail.com>
2012-03-17 14:52:23 +0000

    Adding view files to the User module and referencing the views from the controller

--- modules/User/Controller/Manage.php
+++ modules/User/Controller/Manage.php
@@ -4,13 +4,11 @@ use PPI\Module\Controller as BaseController;
 class Manage extends BaseController {
 	
 	function indexAction() {
-		return 'index';
+		return $this->render('User:manage:index.html.php');
 	}
 	
 	function createAction() {
-		
 		$content = 'Create Action Is Here';
-		
 		return $content;
 		
 	}
