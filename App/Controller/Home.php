a040d79f02848b8a9ec0cd1d9a867e77c2868309
Paul Dragoonis <dragoonis@gmail.com>
2012-04-02 12:57:09 +0100

    Removing content data lookups for the homepage

--- App/Controller/Home.php
+++ App/Controller/Home.php
@@ -2,18 +2,9 @@
 namespace App\Controller;
 class Home extends Application {
 	
-	function preDispatch() {
-		
-	}
-
 	function index() {
-		
-		$cs           = $this->getContentStorage();
-		$titleContent = $cs->getContentByTitle('home_title');
-		$mainContent  = $cs->getContentByTitle('home_content');
-		
 		$this->addCSS('home');
-		$this->render('home/index', compact('titleContent', 'mainContent'));
+		$this->render('home/index');
 	}
 	
 }
