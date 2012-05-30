a7ce5ee9052fd4746d8e32691e0d3c757fb34061
Paul Dragoonis <dragoonis@gmail.com>
2012-05-30 18:28:32 +0100

    Moving htaccess and index.php into /public/. - Adding in an Application module - Cleaning up User module moving parts into Application module - Updating the reademe's installation instructions to reflect composer installation

---
+++ modules/Application/Controller/Index.php
@@ -0,0 +1,38 @@
+<?php
+namespace Application\Controller;
+
+use PPI\Module\Controller as BaseController;
+
+class Index extends BaseController {
+	
+	public function indexAction() {
+		
+		return $this->render('Application:index:index.html.php');
+	}
+	
+	public function indextwigAction() {
+		return $this->render('Application:index:index.html.twig');
+	}
+	
+	public function createAction() {
+		
+		if($this->is('post') && $this->is('ajax')) {
+			$us = $this->getUserStorage();
+			$post = $this->post();
+			$us->insert(array(
+				'name'             => $post['name'],
+				'email'            => $post['email'],
+				'enabled'          => 1,
+				'ip_address'       => $this->getIP(),
+				'referrer_user_id' => $this->session('referrerUserID')
+			));
+		}
+		
+		$this->redirect($this->generateUrl('Homepage'));
+	}
+	
+	protected function getUserStorage() {
+		return new \User\Storage\User($this->getService('DataSource'));
+	}
+	
+}
