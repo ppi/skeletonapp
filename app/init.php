8104192864a0dc76e46dcfe41c653fc28e589152
Paul Dragoonis <dragoonis@gmail.com>
2012-07-12 21:56:35 +0100

    [Core] Ticket: #40 - Deprecation of the PPI folder on the skeletonapp. composer.json moved to the root and init.php is now in ./app/init.php

--- app/init.php
+++ app/init.php
@@ -4,13 +4,13 @@ defined('PPI_VERSION')     || define('PPI_VERSION', '2.0');
 defined('DS')              || define('DS', DIRECTORY_SEPARATOR);
 defined('PPI_PATH')        || define('PPI_PATH', realpath(__DIR__) . '/vendor/ppi/ppi/');
 
-$composerPath = __DIR__ . '/vendor/autoload.php';
+$composerPath = dirname(__DIR__) . '/vendor/autoload.php';
 if(!file_exists($composerPath)) {
 	die('Unable to find composer generated file at: ' . $composerPath);
 }
 
 // Composer generated file include
-$composer = require 'vendor/autoload.php';
+$composer = require dirname(__DIR__) . '/vendor/autoload.php';
 
 // Adding PPI autoloader so modules may add themself to the autoload process on-the-fly
 PPI\Autoload::config(array(
