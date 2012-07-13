583080f7356b243b30c81adbb015024b1768b622
Vítor Brandão <noisebleed@noiselabs.org>
2012-07-13 23:12:40 +0100

    [cs-fix] Code standards fix using php-cs-fixer

--- modules/Application/Module.php
+++ modules/Application/Module.php
@@ -2,35 +2,37 @@
 namespace Application;
 
 use PPI\Module\RoutesProviderInterface,
-	PPI\Module\Module as BaseModule,
-	PPI\Autoload,
-	PPI\Module\Service;
+    PPI\Module\Module as BaseModule,
+    PPI\Autoload,
+    PPI\Module\Service;
 
+class Module extends BaseModule
+{
+    protected $_moduleName = 'Application';
 
-class Module extends BaseModule {
-	
-	protected $_moduleName = 'Application';
-	
-	function init($e) {
-		Autoload::add(__NAMESPACE__, dirname(__DIR__));
-	}
-	
-	/**
-	 * Get the routes for this module
-	 * 
-	 * @return \Symfony\Component\Routing\RouteCollection
-	 */
-	public function getRoutes() {
-		return $this->loadYamlRoutes(__DIR__ . '/resources/config/routes.yml');
-	}
-	
-	/**
-	 * Get the configuration for this module
-	 * 
-	 * @return array
-	 */
-	public function getConfig() {
-		return $this->loadYamlConfig(__DIR__ . '/resources/config/config.yml');
-	}
-	
+    public function init($e)
+    {
+        Autoload::add(__NAMESPACE__, dirname(__DIR__));
+    }
+
+    /**
+     * Get the routes for this module
+     *
+     * @return \Symfony\Component\Routing\RouteCollection
+     */
+    public function getRoutes()
+    {
+        return $this->loadYamlRoutes(__DIR__ . '/resources/config/routes.yml');
+    }
+
+    /**
+     * Get the configuration for this module
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return $this->loadYamlConfig(__DIR__ . '/resources/config/config.yml');
+    }
+
 }
