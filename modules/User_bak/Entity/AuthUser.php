b9b4cb174a83cebda193d97e02d726268ecd7117
Paul Dragoonis <dragoonis@gmail.com>
2012-08-15 16:33:49 +0100

    Removing user_bak module. Accidentally made its way into the repo

--- modules/User_bak/Entity/AuthUser.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace User\Entity;
-
-use User\Entity\User as BaseUser;
-
-class AuthUser extends BaseUser {}
--- modules/User_bak/Storage/Base.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace User\Storage;
-
-use PPI\DataSource\ActiveQuery;
-
-class Base extends ActiveQuery {}
