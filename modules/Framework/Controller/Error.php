e5f342a167bf1be45ea777a668faba298d1cd0e2
Paul Dragoonis <dragoonis@gmail.com>
2012-09-12 00:47:09 +0100

    [Framework] Adding a dev/production switch for the 404 page. Now you can have useful debugging page for dev mode.

--- modules/Framework/Controller/Error.php
+++ modules/Framework/Controller/Error.php
@@ -5,8 +5,13 @@ use PPI\Module\Controller as BaseController;
 
 class Error extends BaseController {
 
-    public function show404Action() {
-        return $this->render('Framework:error:404.html.php');
+    public function show404Action()
+    {
+        if($this->getEnv() !== 'production') {
+            return $this->render('Framework:error:404_dev.html.php');
+        } else {
+            return $this->render('Framework:error:404.html.php');
+        }
     }
     
 }
