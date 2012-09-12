7ec726843c1a5d09608c8826ca660a49df5ff082
Paul Dragoonis <dragoonis@gmail.com>
2012-09-12 17:13:59 +0100

    Adding in an action view for demonstrating things with the MustacheEngine

--- modules/Application/Controller/Index.php
+++ modules/Application/Controller/Index.php
@@ -10,25 +10,33 @@ class Index extends SharedController
         return $this->render('Application:index:index.html.php');
     }
 
-    public function indextwigAction()
+    public function twigAction()
     {
         return $this->render('Application:index:index.html.twig');
     }
 
-    public function indexsmartyAction()
+    public function smartyAction()
     {
         return $this->render('Application:index:index.html.smarty');
     }
     
-    public function indexWithFlashesAction() {
-//        $this->setFlash('warning', 'This is your warning, don\'t let the dogs out again !!');
-//        $this->setFlash('error', 'This is your error, someone let the dogs out !!');
-        return $this->render('Application:index:index.html.php');
-    }
-
-    protected function getUserStorage()
+    public function mustacheAction()
     {
-        return new \UserModule\Storage\User($this->getService('DataSource'));
+        $name = 'Paul';
+        $names = array(
+           array('fname' => 'PPI', 'lname' => 'User1'), 
+           array('fname' => 'PPI', 'lname' => 'User2'), 
+           array('fname' => 'PPI', 'lname' => 'User3') 
+        );
+        
+        return $this->render('Application:index:index.html.mustache', compact('name', 'names')); 
+    }
+    
+    public function indexWithFlashesAction() {
+        $this->setFlash('warning', 'This is your warning, don\'t let the dogs out again !!');
+//        $this->setFlash('error', 'This is your error, someone let the dogs out !!');
+//        $this->setFlash('success', 'You successfully kept the dogs inside !!');
+        return $this->render('Application:index:index.html.php');
     }
 
 }
