963a817b081f0057b5a9728f7650d82771473793
Paul Dragoonis <dragoonis@gmail.com>
2012-11-07 07:45:00 +0000

    Adding the ppi console, and a skeleton module for the console app

---
+++ app/skeleton/Module.php
@@ -0,0 +1,43 @@
+<?php
+namespace [MODULE_NAME];
+
+use PPI\Module\Module as BaseModule;
+use PPI\Autoload;
+
+class Module extends BaseModule
+{
+    protected $_moduleName = '[MODULE_NAME]';
+
+    public function init($e)
+    {
+        Autoload::add(__NAMESPACE__, dirname(__DIR__));
+    }
+    
+    /**
+     * Get the configuration for this module
+     *
+     * @return array
+     */
+//    public function getConfig()
+//    {
+//        return include(__DIR__ . '/resources/config/config.php');
+//    }
+
+    /**
+     * Get the routes for this module
+     *
+     * @return \Symfony\Component\Routing\RouteCollection
+     */
+    public function getRoutes()
+    {
+        return $this->loadYamlRoutes(__DIR__ . '/resources/config/routes.yml');
+    }
+    
+//    public function getServiceConfig()
+//    {
+//        return array('factories' => array(
+//            
+//        ));
+//    }
+
+}
