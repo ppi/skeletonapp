acf60747cec8feb01d59d1bda1e3618b286306b0
Paul Dragoonis <dragoonis@gmail.com>
2012-11-07 07:45:00 +0000

    Adding the ppi console, and a skeleton module for the console app

---
+++ app/skeleton/resources/views/index/index.html.php
@@ -0,0 +1,13 @@
+<?php $view->extend('::base.html.php');?>
+
+<div class="box">
+    <div class="box-header well">Welcome to your new module: [MODULE_NAME]</div>
+    <div class="box-content">
+        <p>Don't forget to check out some relevant documentation you can do with your module</p>
+        <ul class="unstyled">
+            <li><a href="http://www.ppi.io/docs/modules.html" title="" target="_blank">Modules</a></li>
+            <li><a href="http://www.ppi.io/docs/controllers.html" title="" target="_blank">Controllers</a></li>
+            <li><a href="http://www.ppi.io/docs/templating.html" title="" target="_blank">Templating</a></li>
+        </ul>
+    </div>
+</div>
