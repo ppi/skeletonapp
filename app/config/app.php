9aeee2d282603df12af90138126a40e93cd81010
Paul Dragoonis <dragoonis@gmail.com>
2013-05-10 14:44:02 +0100

    Fixing module paths

--- app/config/app.php
+++ app/config/app.php
@@ -9,15 +9,15 @@ $config['framework'] = array(
             'ga_tracking' => 'UA-XXXXX-X',
         ),
     ),
-    'skeleton_module'   => array(
-        'path'  => './utils/skeleton_module'
+    'skeleton_module' => array(
+        'path' => './utils/skeleton_module'
     )
 );
 
-$config['datasource'] => array(
-    'connections' = require __DIR__ . '/datasource.php'
+$config['datasource'] = array(
+    'connections' => require __DIR__ . '/datasource.php'
 );
 
-$config['modules'] = require __DIR__ . 'modules.php';
+$config['modules'] = require __DIR__ . '/modules.php';
 
 return $config;
