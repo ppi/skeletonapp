95e46cb3727dee3d352f66503c83b271bde7aaea
Paul Dragoonis <dragoonis@gmail.com>
2013-06-15 19:43:30 +0100

    Some cleansup

--- modules/Application/Controller/Index.php
+++ modules/Application/Controller/Index.php
@@ -2,17 +2,16 @@
 namespace Application\Controller;
 
 use Application\Controller\Shared as SharedController;
-use Illuminate\Database\Capsule\Manager as Capsule;
 
 
 class Index extends SharedController
 {
     public function indexAction()
     {
-        // $ds         = $this->getService('datasource');
-        // $conn       = $ds->getConnection('users');
-        // $db         = $conn->selectDatabase('test');
-        // $collection = $db->selectCollection('foo');
+        $ds         = $this->getService('datasource');
+        $conn       = $ds->getConnection('content');
+
+        
         return $this->render('Application:index:index.html.php');
     }
 }
