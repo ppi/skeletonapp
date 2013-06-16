f8da3d4170b8650d475f259cb0c87669276680e0
Paul Dragoonis <dragoonis@gmail.com>
2013-06-16 02:07:11 +0100

    Removing multiple templating engine support by default

--- modules/Application/Controller/Index.php
+++ modules/Application/Controller/Index.php
@@ -8,10 +8,7 @@ class Index extends SharedController
 {
     public function indexAction()
     {
-        $ds         = $this->getService('datasource');
-        $conn       = $ds->getConnection('content');
-
-        
         return $this->render('Application:index:index.html.php');
+
     }
 }
