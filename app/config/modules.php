4d25de9346381525b7a86b5ec9b3d890b3d86c9d
Vítor Brandão <vitor@noiselabs.org>
2013-06-23 01:41:41 +0100

    Add BowerModule, SmartyModule, TwigModule and TwitterBootstrapModule to the config

--- app/config/modules.php
+++ app/config/modules.php
@@ -2,7 +2,11 @@
 return array(
     'active_modules' => array(
         'Framework',
-        'Application'
+        'Application',
+        'PPI\BowerModule',
+        'PPI\SmartyModule',
+        'PPI\TwigModule',
+        'PPI\TwitterBootstrapModule'
     ),
     'module_listener_options' => array(
         'module_paths'      => array('./modules', './vendor')
