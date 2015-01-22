364c1ec10e3986ace8ad8f5e09d2c1dec681d026
David McKay <david@rawkode.com>
2015-01-22 21:30:06 +0000

    Yaml based configuration and Vagrantfile with Dockerfile for easy development environments

--- app/datasource.config.php
+++ app/datasource.config.php
@@ -4,10 +4,10 @@ $connections = array();
 
 $connections['main'] = array(
     'type'   => 'pdo_mysql', // This can be any pdo driver. i.e: pdo_sqlite
-    'host'   => 'localhost',
-    'dbname' => 'ppi2_skeleton',
+    'host'   => 'mysql',
+    'dbname' => 'ppi',
     'user'   => 'root',
-    'pass'   => ''
+    'pass'   => 'ppi'
 );
 
 return $connections; // Very important you must return the connections variable from this script
