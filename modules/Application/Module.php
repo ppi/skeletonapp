caae80a0ad7dc2c07e06589304b0814d38d08659
Paul Dragoonis <paul@ppi.io>
2015-04-13 23:14:19 +0100

    Adding in support for Aura\Router returns from a module

--- modules/Application/Module.php
+++ modules/Application/Module.php
@@ -4,8 +4,13 @@ namespace Application;
 
 use PPI\Module\AbstractModule;
 
+use Aura\Router\RouterFactory as AuraRouterFactory;
+
 class Module extends AbstractModule
 {
+
+    protected $factory;
+    protected $router;
 
     /**
      * Get the routes for this module
@@ -14,7 +19,46 @@ class Module extends AbstractModule
      */
     public function getRoutes()
     {
-        return $this->loadYamlRoutes(__DIR__ . '/resources/config/routes.yml');
+//        return $this->loadYamlRoutes(__DIR__ . '/resources/config/routes.yml');
+        $this->router = $this->newRouter();
+        $routes = $this->loadAuraRoutes();
+        var_dump($routes['during.bar']->path); exit;
+        var_dump($routes); exit;
+    }
+
+    protected function newRouterFactory()
+    {
+        return new AuraRouterFactory();
+    }
+
+    protected function newRouter()
+    {
+        return $this->newRouterFactory()->newInstance('/foo/bar/baz');
+    }
+
+    public function loadAuraRoutes()
+    {
+        $this->router->add('before', '/foo');
+
+        $this->router->attach('during', '/during', function ($router) {
+            $router->setTokens(array('id' => '\d+'));
+            $router->setMethod('GET');
+            $router->setValues(array('zim' => 'gir'));
+            $router->setSecure(true);
+            $router->setWildcard('other');
+            $router->setRoutable(false);
+            $router->setIsMatchCallable(function () {
+            });
+            $router->setGenerateCallable(function () {
+            });
+            $router->add('bar', '/bar');
+        });
+
+        $this->router->add('after', '/baz');
+
+        $routes = $this->router->getRoutes();
+        return $routes;
+
     }
 
     /**
