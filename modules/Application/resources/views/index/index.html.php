c7a2f4ae51d5085f64a4e012a2d870e360f89c2a
Paul Dragoonis <paul@ppi.io>
2015-05-06 22:32:48 +0100

    Landing page work

--- modules/Application/resources/views/index/index.html.php
+++ modules/Application/resources/views/index/index.html.php
@@ -8,4 +8,42 @@
     <h2>Welcome!</h2>
     <p>Congratulations! You have successfully installed a new PPI application.</p>
 
+    <div class="clearfix list-container">
+        <div class="landing-list">
+            <p class="list-title">Documentation</p>
+            <ul>
+                <li><a href="">Getting started</a></li>
+                <li><a href="">Modules</a></li>
+                <li><a href="">Routing</a></li>
+                <li><a href="">Controllers</a></li>
+                <li><a href="">Templating</a></li>
+                <li><a href="">Services</a></li>
+            </ul>
+        </div>
+
+        <div class="landing-list">
+            <p class="list-title">Documentation</p>
+            <ul>
+                <li><a href="">Getting started</a></li>
+                <li><a href="">Modules</a></li>
+                <li><a href="">Routing</a></li>
+                <li><a href="">Controllers</a></li>
+                <li><a href="">Templating</a></li>
+                <li><a href="">Services</a></li>
+            </ul>
+        </div>
+
+        <div class="landing-list">
+            <p class="list-title">Documentation</p>
+            <ul>
+                <li><a href="">Getting started</a></li>
+                <li><a href="">Modules</a></li>
+                <li><a href="">Routing</a></li>
+                <li><a href="">Controllers</a></li>
+                <li><a href="">Templating</a></li>
+                <li><a href="">Services</a></li>
+            </ul>
+        </div>
+    </div>
+
 </div>
