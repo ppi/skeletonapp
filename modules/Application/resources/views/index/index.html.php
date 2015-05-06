28ad76fac70d739f22509c40e54c950db4e4509d
Paul Dragoonis <paul@ppi.io>
2015-05-06 23:23:55 +0100

    landing page work

--- modules/Application/resources/views/index/index.html.php
+++ modules/Application/resources/views/index/index.html.php
@@ -12,7 +12,20 @@
         <div class="landing-list">
             <p class="list-title">Documentation</p>
             <ul>
-                <li><a href="">Getting started</a></li>
+                <li><a href="http://ppi.readthedocs.org/en/2.1/book/getting_started.html">Getting started</a></li>
+                <li><a href="http://ppi.readthedocs.org/en/2.1/book/application.html">Skeleton Application</a></li>
+                <li><a href="http://ppi.readthedocs.org/en/2.1/book/modules.html">Modules</a></li>
+                <li><a href="http://ppi.readthedocs.org/en/2.1/book/routing.html">Routing</a></li>
+                <li><a href="http://ppi.readthedocs.org/en/2.1/book/controllers.html">Controllers</a></li>
+                <li><a href="http://ppi.readthedocs.org/en/2.1/book/templating.html">Templating</a></li>
+                <li><a href="http://ppi.readthedocs.org/en/2.1/book/services.html">Services</a></li>
+            </ul>
+        </div>
+
+        <div class="landing-list">
+            <p class="list-title">Configuration</p>
+            <ul>
+                <li><a href="/check.php">Check your environment</a></li>
                 <li><a href="">Modules</a></li>
                 <li><a href="">Routing</a></li>
                 <li><a href="">Controllers</a></li>
@@ -21,29 +34,6 @@
             </ul>
         </div>
 
-        <div class="landing-list">
-            <p class="list-title">Documentation</p>
-            <ul>
-                <li><a href="">Getting started</a></li>
-                <li><a href="">Modules</a></li>
-                <li><a href="">Routing</a></li>
-                <li><a href="">Controllers</a></li>
-                <li><a href="">Templating</a></li>
-                <li><a href="">Services</a></li>
-            </ul>
-        </div>
-
-        <div class="landing-list">
-            <p class="list-title">Documentation</p>
-            <ul>
-                <li><a href="">Getting started</a></li>
-                <li><a href="">Modules</a></li>
-                <li><a href="">Routing</a></li>
-                <li><a href="">Controllers</a></li>
-                <li><a href="">Templating</a></li>
-                <li><a href="">Services</a></li>
-            </ul>
-        </div>
     </div>
 
 </div>
