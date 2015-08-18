2d1d5d95ff9dafd7dd88936f0c5e04d01d9e16d1
Paul Dragoonis <paul@ppi.io>
2015-08-18 05:30:43 +0100

    Cleaning up the landing page

--- public/js/home.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function($) {
-    $('.find-out-more').on('click', function(event){
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $('#landing-two').position().top
-        }, 1500, 'easeInOutCubic');
-    });
-});
