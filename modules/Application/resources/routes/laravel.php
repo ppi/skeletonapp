bdd3126f40cab99707f573153879d07097226206
Paul Dragoonis <dragoonis@gmail.com>
2015-08-22 17:34:04 -0400

    Correcting laravel routes

--- modules/Application/resources/routes/laravel.php
+++ modules/Application/resources/routes/laravel.php
@@ -7,4 +7,5 @@ use PPI\Framework\Router\LaravelRouter;
  */
 $router->get('/', function() { return 'HELLO LARAVEL USER'; });
 
+//$router->get('/laravel', ['as' => 'Homepage', 'uses' => 'Application\Controller\Index@index']);
 $router->get('/laravel', ['as' => 'Homepage', 'Application\Controller\Index@index']);
