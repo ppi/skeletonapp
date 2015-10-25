6a8dd351a47e07c7549fb774037a975d4c62cf11
Paul Dragoonis <dragoonis@gmail.com>
2015-10-25 18:41:29 +0000

    Updating 404 handling

--- modules/Framework/src/Controller/Error.php
+++ modules/Framework/src/Controller/Error.php
@@ -2,15 +2,16 @@
 namespace Framework\Controller;
 
 use PPI\Framework\Module\Controller as BaseController;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class Error extends BaseController {
 
     public function show404Action()
     {
         if($this->getEnv() !== 'production') {
-            return $this->render('Framework:error:404_dev.html.php');
+            throw new ResourceNotFoundException('Page not found');
         } else {
-            return $this->render('Framework:error:404.html.php');
+            return $this->render('Framework:error:404_dev.html.php');
         }
     }
     
