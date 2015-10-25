2bd0bda9707786fd7252512e0aadbfb0fca22a99
Paul Dragoonis <dragoonis@gmail.com>
2015-10-25 19:04:07 +0000

    removing excess aura routes

--- modules/Application/resources/routes/aura.php
+++ modules/Application/resources/routes/aura.php
@@ -6,26 +6,4 @@ $router->add('Homepage', '/')
         'controller' => 'Application\Controller\Index'
     ));
 
-// add a named route with an extended specification
-$router->add('blog.read', '/blog/read/{id}{format}')
-    ->addTokens(array(
-        'id'     => '\d+',
-        'format' => '(\.[^/]+)?',
-    ))
-    ->addValues(array(
-        'controller' => 'Application:Index:index',
-        'format'     => '.html',
-    ));
-    
-// add a named route with an extended specification
-$router->add('blog.view', '/blog/view/{id}{format}')
-    ->addTokens(array(
-        'id'     => '\d+',
-        'format' => '(\.[^/]+)?',
-    ))
-    ->addValues(array(
-        'controller' => new \Application\Controller\Index(),
-        'format'     => '.html',
-    ));
-
 return $router;
