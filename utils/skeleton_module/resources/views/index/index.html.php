4f4c09e3cebdc6b50128216af13b9e00932ecf1b
Paul Dragoonis <dragoonis@gmail.com>
2015-10-26 01:16:47 +0000

    Cleanup of skeleton module. and base twig template

--- utils/skeleton_module/resources/views/index/index.html.php
+++ utils/skeleton_module/resources/views/index/index.html.php
@@ -1,14 +1,4 @@
 <?php $view->extend('::base.html.php');?>
 
-<div class="box">
-    <div class="box-header well">Welcome to your new module: [MODULE_NAME]</div>
-    <div class="box-content">
-        <p>Don't forget to check out some relevant documentation you can do with your module</p>
-        <ul class="unstyled">
-            <li><a href="http://www.ppi.io/docs/2.1/modules.html" title="" target="_blank">Modules</a></li>
-            <li><a href="http://www.ppi.io/docs/2.1/controllers.html" title="" target="_blank">Controllers</a></li>
-            <li><a href="http://www.ppi.io/docs/2.1/routing.html" title="" target="_blank">Routing</a></li>
-            <li><a href="http://www.ppi.io/docs/2.1/templating.html" title="" target="_blank">Templating</a></li>
-        </ul>
-    </div>
-</div>
+
+<div style="color: white">Welcome to your new module: [MODULE_NAME]</div>
--- utils/skeleton_module/src/Controller/Index.php
+++ utils/skeleton_module/src/Controller/Index.php
@@ -2,10 +2,11 @@
 namespace [MODULE_NAME]\Controller;
 
 use [MODULE_NAME]\Controller\Shared as SharedController;
+use Psr\Http\Message\RequestInterface;
 
 class Index extends SharedController
 {
-    public function indexAction()
+    public function indexAction(RequestInterface $request)
     {
         return $this->render('[MODULE_NAME]:index:index.html.php');
     }
