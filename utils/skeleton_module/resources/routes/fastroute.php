0bdd0b3cfc7b80e315c252d1a1a984f6dbafb5b7
Paul Dragoonis <dragoonis@gmail.com>
2015-11-28 03:07:34 +0000

    Adding fastrouter support to skeleton module

--- utils/skeleton_module/resources/routes/fastroute.php
+++ utils/skeleton_module/resources/routes/fastroute.php
@@ -3,4 +3,4 @@
 /**
  * @var \FastRoute\RouteCollector $r
  */
-$r->addRoute('GET', '[MODULE_NAME]/index', '[MODULE_NAME]\Controller\Index');
+$r->addRoute('GET', '/[MODULE_NAME]/index', '[MODULE_NAME]\Controller\Index');
