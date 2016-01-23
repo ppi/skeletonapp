dcf5aa8e7911f706311c396e50b2b42cce3c96b2
Ross Moroney <code.ph0y@googlemail.com>
2016-01-23 16:21:25 +0000

    Fix: Moving code from Parent to Child Storage Class

--- modules/Application/src/Storage/DoctrineDbalBase.php
+++ modules/Application/src/Storage/DoctrineDbalBase.php
@@ -66,30 +66,6 @@ class DoctrineDbalBase
     }
 
     /**
-     * Get a entity by its ID
-     *
-     * @param $id
-     * @return mixed
-     * @throws \Exception
-     */
-    public function getByID($id)
-    {
-        $row = $this->ds->createQueryBuilder()
-            ->select('t.*')
-            ->from($this->getTableName(), 't')
-            ->where('t.id = :id')
-            ->setParameter(':id', $id)
-            ->execute()
-            ->fetch($this->getFetchMode());
-
-        if ($row === false) {
-            throw new \Exception('Unable to obtain row for id: ' . $id);
-        }
-
-        return new Entity($row);
-    }
-
-    /**
      * Delete a row by $id
      *
      * @param  string $id
@@ -123,28 +99,6 @@ class DoctrineDbalBase
     }
 
     /**
-     * Get entity objects from all users rows
-     *
-     * @return array This will return an array of Entity.
-     */
-    public function getAll()
-    {
-        $entities = array();
-
-        $rows = $this->ds->createQueryBuilder()
-            ->select('*')
-            ->from($this->getTableName(), 't')
-            ->execute()
-            ->fetchAll($this->getFetchMode());
-
-        foreach ($rows as $row) {
-            $entities[] = new Entity($row);
-        }
-
-        return $entities;
-    }
-
-    /**
      * Count all the records in table.
      *
      * @return int This will return the count.
@@ -158,43 +112,5 @@ class DoctrineDbalBase
             ->fetch($this->getFetchMode());
 
         return $row['total'];
-    }
-
-    /**
-     * Search table using search fields and value
-     *
-     * @param array $fields The fields in the table you want to search.
-     * @param string $value The value you want to search for.
-     * @param string $filter_type The type of search you want to run.
-     * @return array This will return an array of Entity.
-     */
-    public function search($fields, $value, $filter_type = 'search')
-    {
-        switch ($filter_type) {
-            case 'search':
-                $entities = array();
-                $qb = $this->ds->createQueryBuilder();
-
-                $qb->select('*')
-                   ->from($this->getTableName(), 't');
-
-                foreach ($fields as $field) {
-                    $qb->orWhere('t.' . $field . ' LIKE :' . $field)
-                       ->setParameter(':' . $field, "%" . $value . "%");
-                }
-
-                $qb->execute();
-                $rows = $qb->execute();
-
-                foreach ($rows as $row) {
-                    $entities[] = new Entity($row);
-                }
-
-                return $entities;
-            break;
-            default:
-                throw new \Exception('Filter Type not supported!');
-                break;
-        }
     }
 }
