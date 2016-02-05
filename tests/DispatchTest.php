d0cc403d518099cc72fd6211c5d97e94942447a0
Paul Dragoonis <dragoonis@gmail.com>
2016-02-05 19:52:33 +0000

    Initial test to verify 200 ok from / path

---
+++ tests/DispatchTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @copyright  Copyright (c) 2011-2015 Paul Dragoonis <paul@ppi.io>
+ * @license    http://opensource.org/licenses/mit-license.php MIT
+ *
+ * @link       http://www.ppi.io
+ */
+
+use PPI\Framework\Http\Request as HttpRequest;
+use PPI\Framework\Http\Response as HttpResponse;
+use PPI\Framework\App;
+
+/**
+ * Class AppTest.
+ *
+ * @author Paul Dragoonis <paul@ppi.io>
+ */
+class DispatchTest extends \PHPUnit_Framework_TestCase
+{
+
+	public function testIndex()
+	{
+		// Setup autoloading and include PPI
+		require_once realpath(__DIR__ . '/../app/init.php');
+
+		// Create...
+		$app = new PPI\Framework\App(array(
+			'environment' => 'dev',
+		    'rootDir' => realpath(__DIR__.'/../app'),
+		));
+
+		$app->loadConfig($app->getEnvironment() . '/app.php');
+
+		$request = HttpRequest::create('/');
+		$response = HttpResponse::create();
+
+		$response = $app->dispatch($request, $response);
+
+		$this->assertEquals(200, $response->getStatusCode()); exit;
+	}
+
+}
