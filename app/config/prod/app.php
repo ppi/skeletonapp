4fbc5d707a1ad833eda89c5a43ba103e5597c923
Paul Dragoonis <dragoonis@gmail.com>
2016-02-05 19:58:12 +0000

    Adding a path / test for in prod mode

--- app/config/prod/app.php
+++ app/config/prod/app.php
@@ -3,5 +3,4 @@
 $config = require __DIR__.'/../base/app.php';
 $config['module_listener_options']['config_cache_enabled']     = true;
 $config['module_listener_options']['module_map_cache_enabled'] = true;
-var_dump($config); exit;
 return $config;
--- tests/DispatchTest.php
+++ tests/DispatchTest.php
@@ -20,7 +20,7 @@ use PPI\Framework\App;
 class DispatchTest extends \PHPUnit_Framework_TestCase
 {
 
-	public function testIndex()
+	public function testDispatch()
 	{
 		// Setup autoloading and include PPI
 		require_once realpath(__DIR__ . '/../app/init.php');
@@ -38,7 +38,30 @@ class DispatchTest extends \PHPUnit_Framework_TestCase
 
 		$response = $app->dispatch($request, $response);
 
-		$this->assertEquals(200, $response->getStatusCode()); exit;
+		$this->assertEquals(200, $response->getStatusCode());
 	}
+
+	public function testDispatchProd()
+	{
+		// Setup autoloading and include PPI
+		require_once realpath(__DIR__ . '/../app/init.php');
+
+		// Create...
+		$app = new PPI\Framework\App(array(
+			'environment' => 'prod',
+		    'rootDir' => realpath(__DIR__.'/../app'),
+		));
+
+		$app->loadConfig($app->getEnvironment() . '/app.php');
+
+		$request = HttpRequest::create('/');
+		$response = HttpResponse::create();
+
+		$response = $app->dispatch($request, $response);
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+
 
 }
