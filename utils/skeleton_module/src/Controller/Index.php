855f906443a7b6a8daadf78d8c3075e467971fe2
Paul Dragoonis <dragoonis@gmail.com>
2016-02-11 13:37:33 +0000

    Updating skeleton module for PPI request instead of PSR-7

--- utils/skeleton_module/src/Controller/Index.php
+++ utils/skeleton_module/src/Controller/Index.php
@@ -2,11 +2,11 @@
 namespace [MODULE_NAME]\Controller;
 
 use [MODULE_NAME]\Controller\Shared as SharedController;
-use Psr\Http\Message\RequestInterface;
+use PPI\Framework\Http\Request;
 
 class Index extends SharedController
 {
-    public function indexAction(RequestInterface $request)
+    public function indexAction(Request $request)
     {
         return $this->render('[MODULE_NAME]:index:index.html.[TPL_ENGINE_EXT]');
     }
