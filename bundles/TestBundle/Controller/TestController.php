51f39144167eb5ecc9ba679dc03e536f4e3fce73
Paul Dragoonis <dragoonis@gmail.com>
2016-05-07 11:27:46 +0100

    updating test bundle

--- bundles/TestBundle/Controller/TestController.php
+++ bundles/TestBundle/Controller/TestController.php
@@ -13,9 +13,27 @@ namespace TestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
 
 class TestController extends Controller
 {
+
+
+    /**
+     * This is the documentation description of your method, it will appear
+     * on a specific pane. It will read all the text until the first
+     * annotation.
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="This is a description of your API method",
+     *  filters={
+     *      {"name"="a-filter", "dataType"="integer"},
+     *      {"name"="another-filter", "dataType"="string", "pattern"="(foo|bar) ASC|DESC"}
+     *  }
+     * )
+     */
     public function indexAction()
     {
         return new Response('<h1>Test is working</h1>');
